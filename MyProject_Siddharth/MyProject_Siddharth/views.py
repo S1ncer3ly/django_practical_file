04ccc25ef1af8a561031decdf7446b9436a16e0d
S1ncer3ly <Siddharthchaudhary611@gmail.com>
2023-06-17 15:50:53 +0530

    A new greet text on /greet

--- MyProject_Siddharth/MyProject_Siddharth/views.py
+++ MyProject_Siddharth/MyProject_Siddharth/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def greet(request):
     return HttpResponse("Hello There ! ")
+
+
+def home(request):
+    return render(request, 'home.html')
