fe76125be9a43ed9196a29318108dd591a7e41bb
S1ncer3ly <Siddharthchaudhary611@gmail.com>
2023-06-17 17:11:53 +0530

    added my form

--- MyProject_Siddharth/MyProject_Siddharth/urls.py
+++ MyProject_Siddharth/MyProject_Siddharth/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path('greet/', views.greet, name="greet"),
-    path('child/', views.child, name="child")
+    path('child/', views.child, name="child"),
+    path('form/', views.my_form_view, name='my_form'),
 ]
--- MyProject_Siddharth/MyProject_Siddharth/views.py
+++ MyProject_Siddharth/MyProject_Siddharth/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .form import MyForm
 
 
-def greet(request):
+def greet():
     return HttpResponse("Hello There ! ")
 
 
@@ -12,3 +13,21 @@ def home(request):
 
 def child(request):
     return render(request, 'child.html')
+
+
+def my_form_view(request):
+    if request.method == 'POST':
+        form = MyForm(request.POST)
+        if form.is_valid():
+            # Process the form data
+            member_id = form.cleaned_data['member_id']
+            member_email = form.cleaned_data['member_email']
+            course = form.cleaned_data['course']
+            staff = form.cleaned_data['staff']
+
+            # Redirect to a success page
+            return render(request, 'success.html')
+    else:
+        form = MyForm()
+
+    return render(request, 'my_form.html', {'form': form})
